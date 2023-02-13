13835b2cceacbf52ea1a4bed523aebe8d58606bb
sheilaowiso <49476957+sheilaowiso@users.noreply.github.com>
2023-02-13 18:24:32 +0300

    NoSQL

---
+++ 0x01-NoSQL/11-schools_by_topic.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""list of collection"""
+
+
+def schools_by_topic(mongo_collection, topic):
+    """
+    returns list of school that having same topic
+    """
+    return list(mongo_collection.find({"topics": topic}))
